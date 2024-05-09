8fe60a161b1e34d386c16e6a782f24fa03a418f5
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-05-09 17:10:00 -0600

    added pages

---
+++ app/Products/page.js
@@ -0,0 +1,16 @@
+import { Header } from '@/components/header';
+import { SidePane } from '@/components/side-pane';
+
+export default function Page() {
+    return (
+        <div className="flex flex-col h-screen">
+            <Header className="w-full" />
+            <div className="flex flex-1 overflow-hidden">
+                <SidePane className="w-64 h-full overflow-y-auto" />
+                <main className="flex-1 overflow-y-auto p-5">
+                    <h1>Products</h1>
+                </main>
+            </div>
+        </div>
+    );
+}
