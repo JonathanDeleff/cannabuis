8ce7aae7f5d2b0912b09f025fc824090b38f3a11
Jonathan Deleff <130598106+JonathanDeleff@users.noreply.github.com>
2024-05-29 08:55:52 -0600

    added dashboard link

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Page() {
     return (
         <div>
-            <h1>Home</h1>
+            <Link href='/dashboard'>dashboard</Link>
         </div>
     );
 }
+
