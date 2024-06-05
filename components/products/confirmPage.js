5446c1e0e30848d0d2ec3cc7591c660f90002f4b
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-06-05 09:34:29 -0600

    Clear cart button

--- components/products/confirmPage.js
+++ components/products/confirmPage.js
@@ -1,10 +1,10 @@
 const Confirm = ({ onConfirm, onCancel }) => {
     return (
-        <div>
-            <button className="p-2.5 bg-green-500 text-white rounded-lg" onClick={() => onCancel()} >
+        <div className="flex flex-col items-center">
+            <button className="mt-1 p-2.5 bg-green-500 text-white rounded-lg" onClick={() => onCancel()} >
                 Cancel Sell
             </button>
-            <button className="p-2.5 bg-red-500 text-white rounded-lg" onClick={() => onConfirm()} >
+            <button className="mt-1 p-2.5 bg-red-500 text-white rounded-lg" onClick={() => onConfirm()} >
                 Confirm Sell
             </button>
         </div>
