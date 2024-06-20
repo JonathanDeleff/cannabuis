62b11141ed633904da7168d10c40ae9041b6a645
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-06-20 12:27:45 -0600

    added a function for sql to import instead of using the same code a billion times for api

---
+++ app/lib/utils.js
@@ -0,0 +1,10 @@
+import postgres from "postgres";
+
+export default sql = postgres({
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+    ssl: 'require',    
+});
