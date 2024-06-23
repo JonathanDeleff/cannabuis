7dac09d0893cf6defd60feadfd8b9edf69664aac
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-06-23 03:14:26 -0600

    Update route.js

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -12,12 +12,12 @@ const sql = postgres({
 export async function GET(req) {
     try {
         const data = await sql`SELECT 
-        c_orders.order_id, 
-        c_orders.order_date, 
+        c_order.order_id, 
+        c_order.order_date, 
         c_order_status.status_name, 
         c_order_details.order_cost
     FROM 
-        c_orders
+        c_order
     JOIN 
         c_order_details ON c_orders.order_id = c_order_details.order_id
     JOIN
