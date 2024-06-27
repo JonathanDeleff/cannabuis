dd905e3816f8591a17bf34224416e6eaaae83e3a
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-06-27 00:03:39 -0600

    api for getting order details

---
+++ app/api/orders/getdetails/route.js
@@ -0,0 +1,44 @@
+import postgres from "postgres";
+
+const sql = postgres({
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+    ssl: 'require',    
+});
+
+export async function GET(req) {
+    try {
+        const url = new URL(req.url);
+        const orderId = url.searchParams.get('order_id');
+
+        if (!orderId) {
+            return new Response(JSON.stringify({ error: 'Order ID is required' }), {
+                status: 400,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        const data = await sql`SELECT * FROM c_order_details WHERE order_id = ${orderId}`;
+
+        if (data.length === 0) {
+            return new Response(JSON.stringify({ error: 'Product not found' }), {
+                status: 404,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        return new Response(JSON.stringify(data[0]), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error('Database query error:', error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
