af1132290f804bc17009627cc3701ccd054e1592
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-07-11 17:25:10 -0600

    this time for real

--- app/components/order/confirmOrder.tsx
+++ app/components/order/confirmOrder.tsx
@@ -1,4 +1,9 @@
-export default function Confirm({ onConfirm, onCancel }) {
+interface ConfirmProps {
+    onConfirm: () => void;
+    onCancel: () => void;
+}
+
+const Confirm:React.FC<ConfirmProps> = ({ onConfirm, onCancel }) => {
     return (
         <div className="flex flex-col items-center">
             <button className="mt-1 p-2.5 bg-red-500 text-white rounded-lg" onClick={() => onCancel()} >
@@ -10,4 +15,6 @@ export default function Confirm({ onConfirm, onCancel }) {
         </div>
     );
 }
+
+export default Confirm;
 
