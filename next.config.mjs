25a688bbacf75a17f4e82e04964f2e0326b60c38
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-07-23 14:30:43 -0600

    try this

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,13 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 export default {
-  distDir: 'out', // Ensure Next.js builds to the 'out' directory
+  distDir: 'out', // Ensure the output directory is correctly specified
+  trailingSlash: true, // Optional: ensures paths have trailing slashes, useful for static sites
+  // Other Next.js configurations
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('chrome-aws-lambda');
     }
-
     return config;
   },
 };
