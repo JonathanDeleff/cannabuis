4238728c66c46b316ceecd8ca1678d5d954a0034
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-07-26 16:28:48 -0600

    using deploy to test

--- app/dashboard/returns/page.tsx
+++ app/dashboard/returns/page.tsx
@@ -1,17 +1,23 @@
 "use client"
 import { useState, useEffect } from "react";
 import { ProductType } from "@/app/types/dashboardTypes/types";
+import  Return from "@/app/components/returns/return";
+import { Search } from "@/app/components/dashboard/search";
 
 const ReturnsPage = () => {
     
     const [products, setProducts] = useState<ProductType[]>([]);
+    const [filteredProducts, setFilteredProducts] = useState<ProductType[]>([]);
     const [session, setSession] = useState<any>(null);
     const [searchQuery, setSearchQuery] = useState<string>('');
     
-
+    // Use effect to fetch the products and set the session pulling employee info for returns
     useEffect(() => {
         
+        // Simple try catch, fetch's from our api to grab the session data
+        // Sets session data to session state
         const fetchSession = async () => {
+            console.log('Fetching session...');
             try {
                 const response = await fetch('/api/session');
                 if (!response.ok) throw new Error('Network response was not ok');
@@ -22,7 +28,10 @@ const ReturnsPage = () => {
               }
         }
         
+        // Simple fetch works much the same as the session fetch
+        // Sets the products to products state
         const fetchProducts = async () => {
+            console.log('Fetching products...');
             try {
                 const response = await fetch('/api/products', {
                     method: 'GET',
@@ -38,15 +47,29 @@ const ReturnsPage = () => {
                 console.error('Error fetching products:', error);
             }
         };
-
+        // Call the fetch functions
         fetchSession();
         fetchProducts();
       }, []);
+
+      useEffect(() => {
+        if (searchQuery) {
+            const filtered = products.filter(product =>
+                product.product_title.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setFilteredProducts(filtered);
+        } else {
+            setFilteredProducts([]);
+        }
+    }, [searchQuery, products]);
     
     return (
-        <main className="flex gap mt-5 flex-col">
-            <div className="bg-bgSoft p-5 rounded-lg mt-5 max-h-4/5 w-full">
-                <h1>Returns</h1>
+        <main className="bg-bgSoft p-5 rounded-lg mt-5 max-h-full w-full">
+            <div className="flex">
+                <Search placeholder="Search for a product" setSearchQuery={setSearchQuery} />
+            </div>
+            <div className="flex">
+            <Return products={filteredProducts}/>
             </div>
         </main>
     );
